08173f49010ebcf3e8b2276a575e9eabafcb9f65
David Dias <daviddias.p@gmail.com>
2013-11-14 02:42:13 +0000

    add relative paths

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  var path = req.url || '' 
   res.writeHead(301,
-    {Location: 'http://livebots.azurewebsites.net/'}
+    {Location: 'http://livebots.azurewebsites.net' + path}
   );
   res.end();
 }).listen(process.env.PORT || 8080);
